7ac6d78dc237d0fa90a498c4255b3c6888ad1164
DanielMiguelez <danielmiguelez1993@gmail.com>
2023-01-19 13:56:01 +0100

    Second commit

--- models/lending.js
+++ models/lending.js
@@ -3,22 +3,22 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Lendings extends Model {
+  class Lending extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Lending.belongsTo(models.Account);
     }
   }
-  Lendings.init({
+  Lending.init({
     AccountId: DataTypes.INTEGER,
     amount: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'Lendings',
+    modelName: 'Lending',
   });
-  return Lendings;
+  return Lending;
 };
